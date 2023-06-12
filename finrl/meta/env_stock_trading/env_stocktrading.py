b45af1b8a7a571eead0f45de5f201aa24849f6c5
mlischke03 <m.lischke@live.de>
2023-06-12 23:05:47 +0200

    reverse value error fix

--- finrl/meta/env_stock_trading/env_stocktrading.py
+++ finrl/meta/env_stock_trading/env_stocktrading.py
@@ -395,14 +395,10 @@ class StockTradingEnv(gym.Env):
         return self.state
 
     def _initiate_state(self):
-        # Ensure self.num_stock_shares has the same length as self.data.close
-        if len(self.num_stock_shares) < len(self.data.close):
-            self.num_stock_shares.extend([0] * (len(self.data.close) - len(self.num_stock_shares)))
-
         if self.initial:
             # For Initial State
             if len(self.df.tic.unique()) > 1:
-                # for multiple stocks
+                # for multiple stock
                 state = (
                     [self.initial_amount]
                     + self.data.close.values.tolist()
@@ -414,7 +410,7 @@ class StockTradingEnv(gym.Env):
                         ),
                         [],
                     )
-                )
+                )  # append initial stocks_share to initial state, instead of all zero
             else:
                 # for single stock
                 state = (
@@ -426,11 +422,13 @@ class StockTradingEnv(gym.Env):
         else:
             # Using Previous State
             if len(self.df.tic.unique()) > 1:
-                # for multiple stocks
+                # for multiple stock
                 state = (
                     [self.previous_state[0]]
                     + self.data.close.values.tolist()
-                    + self.previous_state[(self.stock_dim + 1) : (self.stock_dim * 2 + 1)]
+                    + self.previous_state[
+                        (self.stock_dim + 1) : (self.stock_dim * 2 + 1)
+                    ]
                     + sum(
                         (
                             self.data[tech].values.tolist()
@@ -444,12 +442,12 @@ class StockTradingEnv(gym.Env):
                 state = (
                     [self.previous_state[0]]
                     + [self.data.close]
-                    + self.previous_state[(self.stock_dim + 1) : (self.stock_dim * 2 + 1)]
+                    + self.previous_state[
+                        (self.stock_dim + 1) : (self.stock_dim * 2 + 1)
+                    ]
                     + sum(([self.data[tech]] for tech in self.tech_indicator_list), [])
                 )
-
         return state
-
 
     def _update_state(self):
         if len(self.df.tic.unique()) > 1:
